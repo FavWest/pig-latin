ce87a65e7f7dafaa7c6496cd73455a41e53536d1
Vanessa Su <vnessa.su@gmail.com>
2021-03-31 14:05:14 -0400

    Modify the pigLatin function to pass the vowel word all caps test case.
    Co-authored-by: Tori West <victoria_west@ymail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 function pigLatin(text){
+  if(text.toUpperCase() === text) {
+    return text + "WAY";
+  }
   return text + "way";
 }
 
-const text = "all";
+// const text = "all";
+const text = "ALL";
 console.log(pigLatin(text));
