c6c138b7b32906ade9c129bb2b9526a07e377ab7
Vanessa Su <vnessa.su@gmail.com>
2021-03-31 14:19:44 -0400

    Modify the pigLatin function to pass the translate vowel but not consonant words test case.
    Co-authored-by: Tori West <victoria_west@ymail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
 function pigLatin(text){
   const wordArray=text.split(" ");
+  const vowels = "aeiou";
   let pigLatinText="";
   wordArray.forEach(function(word, index){
-    if(text.toUpperCase() === text) {
-      pigLatinText += word+"WAY";
+    let firstLetter = word[0];
+    if(vowels.includes(firstLetter)){
+      if(text.toUpperCase() === text) {
+        pigLatinText += word+"WAY";
+      } else {
+        pigLatinText += word+"way";
+      }
+    } else {
+      pigLatinText += word;
     }
-    pigLatinText += word+"way";
     if(index<wordArray.length-1){
-      pigLatinText += " ";
-    }
+        pigLatinText += " ";
+      }
   });
   return pigLatinText;
 }
 
 // const text = "all";
 // const text = "ALL";
-const text = "all owls"
+// const text = "all owls"
+const text = "all cats and owls";
 console.log(pigLatin(text));
